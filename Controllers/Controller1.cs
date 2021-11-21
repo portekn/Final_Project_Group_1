bf4dbfaa5f899bb3673714194fb02d4205fd549c
Evan Kelley <kelleyev@mail.uc.edu>
2021-11-21 11:36:31 -0800

    Initial code structuring

---
+++ Controllers/Controller1.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Final_Project_Group_1.controller
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class VariableController : ControllerBase
+    {
+        
+    }
+}
