3032c6723acb2284c7c4c8832e6a2312b356855d
Evan Kelley <kelleyev@mail.uc.edu>
2021-11-21 12:42:01 -0800

    Added Controller getter/setter

--- Controllers/Controller1.cs
+++ Controllers/Controller1.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Final_Project_Group_1;
+
 
 namespace Final_Project_Group_1.controller
 {
@@ -8,5 +10,19 @@ namespace Final_Project_Group_1.controller
     public class Controller1 : ControllerBase
     {
         private readonly ILogger<Controller1> _Logger;
+        private readonly ServiceContext1 _Context;
+        
+        public Controller1(ILogger<Controller1> Logger, ServiceContext1 Context)
+        {
+            _Logger = Logger;
+            _Context = Context;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok();
+        }
     }
 }
