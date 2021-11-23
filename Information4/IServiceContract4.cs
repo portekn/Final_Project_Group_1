e8a236c4fc12b0f3e54b5593dc3a414486c9e165
YifeiYang20 <94498682+YifeiYang20@users.noreply.github.com>
2021-11-24 04:29:35 +0800

    Update IServiceContract4.cs

---
+++ Information4/IServiceContract4.cs
@@ -0,0 +1,14 @@
+using Final_Project_Group_1.Models;
+using System.Collections.Generic;
+
+namespace Final_Project_Group_1.Information4
+{
+    public interface IServiceContract4
+    {
+        Movie AddMovie(Movie movie);
+        List<Movie> GetMovies();
+        void UpdateMovie(Movie movie);
+        void DeleteMovie(string movie);
+        Movie GetMovie(string movie);
+    }
+}
