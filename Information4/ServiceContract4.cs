11f9ba6a52e3842e27cedb33d492bf92346cae08
YifeiYang20 <94498682+YifeiYang20@users.noreply.github.com>
2021-11-24 04:47:18 +0800

    Update ServiceContract4.cs

--- Information4/ServiceContract4.cs
+++ Information4/ServiceContract4.cs
@@ -6,48 +6,48 @@ using System.Linq;
     {  
         public class ServiceContract4: IServiceContract4
         {   
-            public MovieContext _movieContext; 
-            public ServiceContract3 (MovieContext moviescontext)  
+            public MovieContext _movieDbcontext; 
+            public ServiceContract3 (MovieContext movieDbcontext)  
             {
-                _movieContext = moviescontext;
+                _movieDbcontext = movieDbcontext;
             }  
 
         //----------Add Movie----------//
             public Movie AddMovie(Movie movie)  
             {
-                _movieContext.Movies.Add(movie);
-                _movieContext.SaveChanges();  
+                _movieDbcontext.Movies.Add(movie);
+                _movieDbcontext.SaveChanges();  
                 return movie;  
             }
 
         //----------Get Movie----------//
             public List<Movie> GetMovies()  
             {  
-                return _moviesContext.Movies.ToList();  
+                return _movieDbcontext.Movies.ToList();  
             }
 
         //----------Update Movie----------//
             public void UpdateMovie(Movie movie)  
             {
-                _moviesContext.Movies.Update(Movie);
-                _moviesContext.SaveChanges();  
+                _movieDbcontext.Movies.Update(movie);
+                _movieDbcontext.SaveChanges();  
             }
 
         //----------Delete Movie----------//
             public void DeleteMovie(string movie)  
             {  
-                var variable2 = _moviesContext.Movies.FirstOrDefault(x => x.movie == movie);
+                var variable2 = _movieDbcontext.Movies.FirstOrDefault(x => x.movie == movie);
                 if (variable2 != null)  
                 {
-                    _moviesContext.Remove(variable2);
-                    _moviesContext.SaveChanges();  
+                    _movieDbcontext.Remove(variable2);
+                    _movieDbcontext.SaveChanges();  
                 }  
             }
 
         //----------Get Movie----------//
             public Movie GetMovie(string movie)  
             {  
-                return _moviesContext.Movies.FirstOrDefault(x => x.movie == movie);  
+                return _movieDbcontext.Movies.FirstOrDefault(x => x.movie == movie);  
             }  
         }  
  }
