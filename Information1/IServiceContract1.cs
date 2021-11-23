8d73fadeb0d87c05617933d6502ef9f0d043852b
Evan Kelley <kelleyev@mail.uc.edu>
2021-11-22 20:21:56 -0800

    Corrected inproper interface name

--- Information1/IServiceContract1.cs
+++ Information1/IServiceContract1.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace Final_Project_Group_1.Information1  
     {  
-        //Interface name needs to include �I� at the beginning 
-        public interface INameOFServiceContract
-{
+    //Interface name needs to include �I� at the beginning 
+    public interface IServiceContract1
+    {
         Location AddLocation(Location location);
         List<Location> GetLocations();
         void UpdateLocation(Location location);
         void DeleteLocation(int Id);
         Location GetLocation(int Id);
-}
+    }
 }
