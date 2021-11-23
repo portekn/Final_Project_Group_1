4eab23c13f80cf0f8142ca755efabf0b55f390d0
Danae Brenner <danae.brenner@gmail.com>
2021-11-23 18:06:40 -0500

    added notes
    added notes

--- Models/Model4.cs
+++ Models/Model4.cs
@@ -2,14 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Final_Project_Group_1.Models
 {
-
+    //Danae Brenner
     public class Movie
     {
         [Key]
+
         public string movie { get; set; }
+
         public int Year { get; set; }
+
         public string Director { get; set; }
+
         public int Score { get; set; }
+
         public string Type { get; set; }
 
     }
