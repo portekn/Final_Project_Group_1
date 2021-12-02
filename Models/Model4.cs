270321368e9f319e5e7505ff33d8959fa582fc4b
ItzSebas10 <chacnajs@mail.uc.edu>
2021-12-02 18:34:34 -0500

    Idk what changed

--- Models/Model4.cs
+++ Models/Model4.cs
@@ -6,17 +6,11 @@ namespace Final_Project_Group_1.Models
     public class Movie
     {
         [Key]
-
         public int Id { get; set; }
         public string MovieName { get; set; }
-
         public int Year { get; set; }
-
         public string Director { get; set; }
-
         public int Score { get; set; }
-
         public string Type { get; set; }
-
     }
 }
